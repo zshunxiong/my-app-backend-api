7cf266b9880c5d4355d778d5dad90f446e6d910b
teoh1 <teoh@program.com.tw>
2020-05-07 14:00:33 +0800

    test

--- config.js
+++ config.js
@@ -8,20 +8,7 @@ const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_P
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: { 
-    rejectUnauthorized: false 
-  }
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
-
-// db Connection w/ localhost 資料庫連線設定
-// const db = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host: process.env.local_HOST,
-//     user: process.env.local_USER,
-//     password: process.env.local_PASSWORD,
-//     database: process.env.local_DATABASE
-//   }
-// });
 
 module.exports = pool;
