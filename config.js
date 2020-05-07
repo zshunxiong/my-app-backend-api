1bfb3cec94c29e129d8e77fbd3595cf81fe9bc27
teoh1 <teoh@program.com.tw>
2020-05-07 09:25:01 +0800

    connectionString

--- config.js
+++ config.js
@@ -1,24 +1,24 @@
 const express = require('express');
 
 // db Connection w/ Heroku
-// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-// const db = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     connectionString: connectionString,
-//     ssl: true,
-//   }
-// });
-
-// db Connection w/ localhost 資料庫連線設定
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    connectionString: connectionString,
+    ssl: true,
   }
 });
+
+// db Connection w/ localhost 資料庫連線設定
+// const db = require('knex')({
+//   client: 'pg',
+//   connection: {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+//   }
+// });
 
 module.exports = db;
